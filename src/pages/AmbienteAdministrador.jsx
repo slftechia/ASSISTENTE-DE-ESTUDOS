02e75c899f48f9f1136f254060c087185a8412a7
Sandro Fraga <sandro.fraga@pawaresoftwares.onmicrosoft.com>
2025-06-25 19:21:27 +0000

    Deleted src

--- src/pages/AmbienteAdministrador.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AmbienteLayout from './AmbienteLayout';
-
-export default function AmbienteAdministrador() {
-  return (
-    <AmbienteLayout titulo="Ambiente do Administrador Escolar" cargo="administrador" />
-  );
-} 
