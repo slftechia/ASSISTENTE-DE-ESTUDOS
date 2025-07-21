d772ca1687ea1540bd183ae84e5845dd15a3d98a
Sandro Fraga <sandro.fraga@pawaresoftwares.onmicrosoft.com>
2025-07-21 19:33:00 -0300

    Remove marcadores de conflito do main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-=======
 import { AuthProvider } from './contexts/AuthContext';
->>>>>>> aaafbec43ab72ad9305f0e7e0a1e92687b56a87a
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <App />
-  </React.StrictMode>
-); 
-=======
     <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>
 );
->>>>>>> aaafbec43ab72ad9305f0e7e0a1e92687b56a87a
